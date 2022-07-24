84ce395a4f4cd521f70848e0c8f45024392aed25
Raghvendra Singh <raghvendrrsingh@gmail.com>
2022-07-24 21:10:16 +0530

    Backend project setup done

---
+++ index.js
@@ -0,0 +1,15 @@
+const { ApolloServer } = require('apollo-server');
+
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  csrfPrevention: true,
+  cache: 'bounded',
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
